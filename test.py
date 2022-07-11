1bfa6b2dc5ab763e40033c1da87507625bd998e9
Bart <bart.thonissen@gmail.com>
2022-07-12 00:11:07 +0200

    Versie1

---
+++ test.py
@@ -0,0 +1,22 @@
+#from sklearn.datasets import load_boston
+#Boston = load_boston()
+#print(Boston.data.shape)
+
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+
+from sklearn.calibration import CalibratedClassifierCV, CalibrationDisplay
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+
+lr = LogisticRegression(C=1.0)
+gnb = GaussianNB()
+gnb_isotonic = CalibratedClassifierCV(gnb, cv=2, method="isotonic")
+gnb_sigmoid = CalibratedClassifierCV(gnb, cv=2, method="sigmoid")
+
+clf_list = [
+    (lr, "Logistic"),
+    (gnb, "Naive Bayes"),
+    (gnb_isotonic, "Naive Bayes + Isotonic"),
+    (gnb_sigmoid, "Naive Bayes + Sigmoid"),
+]
